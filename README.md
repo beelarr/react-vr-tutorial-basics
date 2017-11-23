ab520aba9204e92970d0ec55e1d207e781ee32c8
beelarr <bryonl@me.com>
2017-11-22 20:19:43 -0600

    Add readme

---
+++ README.md
@@ -0,0 +1,11 @@
+# React VR Tutorial
+
+from [David Katz](http://davidtkatz.com)'s [React VR - Udemy Course](https://www.udemy.com/react-vr)
+* Basics
+
+#### To Setup
+
+```
+$ npm install -g react-vr-cli
+$ npm start
+```
---
+++ sections/events/index.vr.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+import { AppRegistry, asset, Pano, View, Text, } from 'react-vr';
+
+export default class Basics extends Component {
+    constructor() {
+        super();
+        this.state={
+            fontSize: .1
+        }
+    }
+    render() {
+        return (
+            <View>
+                <Pano source={asset('starry-sky.jpg')}></Pano>
+                <Text
+                    onEnter={() => this.setState({fontSize: this.state.fontSize * 3})}
+                    onExit={() => this.setState({fontSize: this.state.fontSize / 3})}
+                    style={{
+                        fontSize: this.state.fontSize,
+                        transform: [{translate: [0, 0, -2.5]}]
+                    }}
+                >Hover Over ME!!!!</Text>
+            </View>
+        )
+    }
+};
+
+AppRegistry.registerComponent('Basics', () => Basics);
