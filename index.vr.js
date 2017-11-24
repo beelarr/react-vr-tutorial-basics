b6595b774b1d80a9ed6430ffa0a79122ca04ed96
beelarr <bryonl@me.com>
2017-11-24 08:20:09 -0600

    Custom model from tinker add

--- index.vr.js
+++ index.vr.js
@@ -1,26 +1,18 @@
 import React, { Component } from 'react';
 
-import { AppRegistry, asset, Pano, View, Box } from 'react-vr';
+import { AppRegistry, asset, Pano, View, Box, Model } from 'react-vr';
 
 export default class Basics extends Component {
     render() {
         return (
             <View>
                 <Pano source={asset('starry-sky.jpg')}></Pano>
-                <Box
-                    dimWidth={0.5}
-                    dimHeight={0.5}
-                    dimDepth={0.5}
-                    wireframe={true}
-                    style={{
-                        color: '#dd2222',
-                        transform: [
-                            {translate: [0, 0, -3]},
-                            {translateY: 1},
-                            {translateX: -0.5},
-                            {rotateZ: 45}
-                        ]
-                    }}
+                <Model
+                    source={{obj:asset('tinker.obj')}}
+                    style={{transform: [
+                        {translate: [0, -1, -3]}
+                    ]}}
+
                 />
             </View>
         )
